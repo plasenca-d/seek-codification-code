c99c7f51a19f5d2c82425f0d38db5832e6758f07
Franzua Plasencia <franzua.15pc@gmail.com>
2024-12-29 23:15:22 -0500

    fix(auth): add mobile first style

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <main className="flex gap-10">
-        <div className="flex-1 p-5">
+        <div className="hidden lg:flex-1 lg:block p-5">
           <AnimatedSide />
         </div>
         <div className="flex-1">
